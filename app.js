92584e0ccd41da767253a271d34b45e6aae0e53f
Shivam Kataria <shivam@sensenuts.com>
2018-12-11 12:51:45 +0530

    First Project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express= require('express');
+const app=express();
+var index=1;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+	//res.send('hello world');
+	res.render('index');
+});
+
+const server=app.listen(3000);
+const io=require('socket.io')(server);
+
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+		console.log('message: ' + msg);
+	});
+});
---
+++ public/chat.js
@@ -0,0 +1,31 @@
+$(function(){
+    var socket=io.connect('http://localhost:3000');
+
+    var message=$('#message');
+    var username=$('#username');
+    var send_message=$('#send_message');
+    var send_username=$('#send_username');
+    var chatroom=$("#chatroom");
+
+    send_message.click(()=>{
+        socket.emit('new_message', {message:message.val()})
+    })
+
+    socket.on("new_message", (data)=>{
+        console.log(data);
+        chatroom.append("<p class='message'>"+ data.username +":"+data.message+"</p>")
+    })
+
+    send_username.click(()=>{
+        console.log(username.val());
+            socket.emit('change_username', {username: username.val()});
+    })
+
+    message.bind("keypress", ()=>{
+        socket.emit('typing');
+    })
+
+    socket.on('typing', (data)=>{
+        feedback.html("<p><i>"+data.username+"is typing..."+"</i></p>");
+    })
+})
